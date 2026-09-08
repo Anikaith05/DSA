cd54f1aa2f55e2c8f0c1b6eef8506aaff5af6fa9
Anikaith <anikaithkamala07@gmail.com>
2026-09-08 20:10:35 +0530

    preorder traversal code

--- trees/basic/preorder_traversal.cpp
+++ trees/basic/preorder_traversal.cpp
@@ -1,2 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    void traversal(TreeNode* root,vector<int>&order){
+        if(root==nullptr){
+            return;
+        }
+        order.push_back(root->val);
+        traversal(root->left,order);
+        traversal(root->right,order);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>order;
+        traversal(root,order);
+        return order;
+    }
+};
