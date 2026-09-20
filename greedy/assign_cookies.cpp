ec08254e5f754b8085f8edf102c794fe2bc6935f
Anikaith <anikaithkamala07@gmail.com>
2026-09-20 15:17:49 +0530

    assign cookies greedy code

---
+++ greedy/assign_cookies.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int gs=g.size();
+        int ss=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0,j=0;
+        int c=0;
+        while(i<gs&&j<ss){
+            if(g[i]<=s[j]){
+                i++;
+                j++;
+                c++;
+            }
+            else{
+                j++;
+            }
+        }
+        return c;
+    }
+};
